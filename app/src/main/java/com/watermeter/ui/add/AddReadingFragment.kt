ded79799646779753d86f71db9eb25962a2b97e0
kozvits <kozvits@gmail.com>
2026-03-25 13:09:58 +0300

    Update AddReadingFragment.kt

--- app/src/main/java/com/watermeter/ui/add/AddReadingFragment.kt
+++ app/src/main/java/com/watermeter/ui/add/AddReadingFragment.kt
@@ -1,11 +1,9 @@
 package com.watermeter.ui.add
 
 import android.Manifest
-import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
