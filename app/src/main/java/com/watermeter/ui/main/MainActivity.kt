37636713c1748f1446e949d9e2a2ffd5f27ea185
kozvits <kozvits@gmail.com>
2026-03-25 11:38:38 +0300

    Add files via upload

--- app/src/main/java/com/watermeter/ui/main/MainActivity.kt
+++ app/src/main/java/com/watermeter/ui/main/MainActivity.kt
@@ -1,1 +1,42 @@
+package com.watermeter.ui.main
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.watermeter.R
+import com.watermeter.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Connect bottom nav with nav controller
+        binding.bottomNav.setupWithNavController(navController)
+
+        // Hide bottom nav on history detail screen
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNav.visibility = when (destination.id) {
+                R.id.historyFragment -> android.view.View.GONE
+                else -> android.view.View.VISIBLE
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
